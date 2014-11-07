3b5a5720a3ce7793280ffcdef324c30b6edd0c38
Namdnitbk92 <Namdnitbk92@gmail.com>
2014-11-07 17:06:59 +0700

    Init Variables use class Books,make get(),set() for attribute

--- LibrariesManagement/src/com/project/LibrariesBook/Books.java
+++ LibrariesManagement/src/com/project/LibrariesBook/Books.java
@@ -5,20 +5,90 @@ package com.project.LibrariesBook;
 
 
 /**
- * @author Nam-Thuy92
+ * @author Namdnitbk92
  *
  */
-public class Books {
+ import java.sql.*;//import cac package can su dung
 
-	/**
-	 * 
-	 */
+public class Books {
+	
+	//khoi tao cac bien private cua class Books
+	private Connection connection = null;
+	private Statement statement = null;
+	private ResultSet resultSet = null;
+
+	private int bookID;
+	private String subject;
+	private String title;
+	private String author;
+	private String publisher;
+	private int copyright;
+	private int edition;
+	private int pages;
+	private String ISBN;
+	private int numberOfBooks;
+	private int numberOfAvailbleBooks;
+	private int numberOfBorrowedBooks;
+	private String library;
+	private boolean availble;
+	private String URL = "jdbc:odbc:JLibrary";
+
 	public Books() {
-		// TODO Auto-generated constructor stub
+	}
+	//Cac phuong thuc get(),set().
+	public int getBookID() {
+		return bookID;
 	}
 
-	/**
-	 * @param args
-	 */
+	public String getSubject() {
+		return subject;
+	}
 
+	public String getTitle() {
+		return title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public String getPublisher() {
+		return publisher;
+	}
+
+	public int getCopyright() {
+		return copyright;
+	}
+
+	public int getEdition() {
+		return edition;
+	}
+
+	public int getPages() {
+		return pages;
+	}
+
+	public String getISBN() {
+		return ISBN;
+	}
+
+	public int getNumberOfBooks() {
+		return numberOfBooks;
+	}
+
+	public int getNumberOfAvailbleBooks() {
+		return numberOfAvailbleBooks;
+	}
+
+	public int getNumberOfBorrowedBooks() {
+		return numberOfBorrowedBooks;
+	}
+
+	public String getLibrary() {
+		return library;
+	}
+
+	public boolean getAvailble() {
+		return availble;
+	}
 }
