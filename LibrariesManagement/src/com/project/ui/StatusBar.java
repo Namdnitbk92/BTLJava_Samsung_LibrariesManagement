1ed8135d44f61879b40b5c9c2c05248dcd95634a
oostarbk <lvdung.bk@gmail.com>
2014-11-18 17:09:13 +0700

    Update StatusBar and ToolBar for program

--- LibrariesManagement/src/com/project/ui/StatusBar.java
+++ LibrariesManagement/src/com/project/ui/StatusBar.java
@@ -1,5 +1,21 @@
 package com.project.ui;
 
-public class StatusBar {
+import javax.swing.*;
+import javax.swing.border.SoftBevelBorder;
+import java.awt.*;
 
+public class StatusBar extends JPanel {
+	
+	/***************************************************************************
+     ***         Khai bao cac bien su dung cho chuong trinh                  ***
+     ***************************************************************************/
+	
+	private JLabel statusBar = new JLabel("  ");
+
+	//Phuong thuc khoi tao cho lop StatusBar
+	public StatusBar() {
+		statusBar.setFont(new Font("Tahoma", Font.BOLD, 9));
+		this.add(statusBar);
+		this.setBorder(new SoftBevelBorder(javax.swing.border.BevelBorder.LOWERED));
+	}
 }
--- LibrariesManagement/src/com/project/ui/Toolbar.java
+++ LibrariesManagement/src/com/project/ui/Toolbar.java
@@ -1,5 +1,36 @@
 package com.project.ui;
 
-public class Toolbar {
+import javax.swing.*;
 
+public class Toolbar extends JToolBar {
+
+	/***************************************************************************
+	 *** Khai bao cac bien su dung cho chuong trinh ***
+	 ***************************************************************************/
+
+	// Khoi tao buttons de su dung tren ToolBar
+	public JButton[] button;
+	// Khoi tao ten file anh hien thi cua cac nut (24*24)
+	public String[] imageName24 = { "images/Add24.gif", "images/List24.gif",
+									"images/Add24.gif", "images/List24.gif",
+									"images/Find24.gif", "images/Export24.gif", 
+									"images/Import24.gif","images/Exit24.gif" };
+	// Khoi tao tipText cho ToolBar
+	public String[] tipText = { "Thêm sách", "Danh sách sách",
+								"Thêm thành viên", "Danh sách thành viên", 
+								"Tìm kiếm", "Mượn sách", 
+								"Trả sách", "Thoát" };
+	//Phuong thuc khoi tao lop ToolBar
+	public Toolbar() {
+		button = new JButton[19];
+		for (int i = 0; i < imageName24.length; i++) {
+			if (i == 2 || i == 4 || i == 5 || i == 7)
+			//Them dau phan cach cho ToolBar
+				addSeparator();
+			// for adding the buttons to toolBar
+			add(button[i] = new JButton(new ImageIcon(ClassLoader.getSystemResource(imageName24[i]))));
+			// for setting the ToolTipText to the button
+			button[i].setToolTipText(tipText[i]);
+		}
+	}
 }
