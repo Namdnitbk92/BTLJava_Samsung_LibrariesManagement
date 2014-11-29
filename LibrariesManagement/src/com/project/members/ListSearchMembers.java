6b7ce177888b91740ae45b1fea73d80e1a555a41
thuanbk090792 <timlaibautroi090792@gmail.com>
2014-11-29 20:04:08 +0700

    Viết code cho lớp ListSearchMembers

--- LibrariesManagement/src/com/project/members/ListSearchMembers.java
+++ LibrariesManagement/src/com/project/members/ListSearchMembers.java
@@ -1,5 +1,27 @@
 package com.project.members;
 
-public class ListSearchMembers {
+import javax.swing.*;
+import javax.swing.table.TableColumn;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.print.PrinterException;
+import java.awt.print.PrinterJob;
+import java.sql.SQLException;
+
+public class ListSearchMembers extends JInternalFrame {
+	//khai bao các biến
+	//tạo North Panel
+	private JPanel northPanel = new JPanel() ;
+	//tạo Center Panel
+	private JPanel centerPanel = new JPanel();
+	//tạo label
+	private JLabel label = new JLabel ("Danh sách tìm kiếm thành viên");
+	//tạo button
+	private JButton printButton ;
+	//tạo table
+	private JTable table ;
+	
 
 }
