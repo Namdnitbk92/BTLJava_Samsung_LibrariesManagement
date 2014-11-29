b23bba6c4d3750578d52c6055c08262cdaffa063
thuanbk090792 <timlaibautroi090792@gmail.com>
2014-11-29 11:31:44 +0700

    Hoàn thành code lớp Members

--- LibrariesManagement/src/com/project/members/Members.java
+++ LibrariesManagement/src/com/project/members/Members.java
@@ -60,23 +60,74 @@ public class Members {
 		return expired ;
 	}
 	
-	pulic void connection (String Query) {
+	public void connection (String Query) {
 		try{
-			Class.forName("sun.jdbc.odbc")
+			Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
+		}
+		catch(ClassNotFoundException e1)
+		{
+			System.out.println("Members.java \n" + e1.toString());
+		}
+		catch(Exception e2)
+		{
+			System.out.println("Members.java\n" +e2.toString());
+		}
+		/* tao connection, tao statement va update bang CSDL. Sau do dong statement
+		 * va connection .Khoi SQLException de bat loi 
+		 * */
+		
+		try
+		{
+			connection = DriverManager.getConnection(URL);
+			statement = connection.createStatement();
+			resultSet = statement.executeQuery(Query);
+			while(resultSet.next()) 
+			{
+				memberID = resultSet.getInt(1);
+				ID = resultSet.getInt(2);
+				password = resultSet.getString(3);
+				name = resultSet.getString(4);
+				email = resultSet.getString(5);
+				major = resultSet.getString(6);
+				numberOfBooks = resultSet.getInt(7);
+				mony = resultSet.getInt(8);
+				expired = resultSet.getDate(9);				
+			}
+			resultSet.close();
+			statement.close();
+			connection.close();
+		}
+		catch(SQLException SQLe) {
+			System.out.println("Members.java\n"+SQLe.toString());
+		}		
+	}
+	public void update (String Query){
+		try{
+			Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");			
+		}
+		catch(ClassNotFoundException e3){
+			System.out.println("Members.java\n"+e3.toString());
+		}
+		catch(Exception e4){
+			System.out.println("Members.java\n"+e4.toString());
+		}
+		
+		/*
+		 * tao ket noi,tao khai bao va cap nhat
+		 * bang co so du lieu.Sau do dong cac khai bao
+		 * va ket noi.Khoi SQLException cho viec bat loi
+		 * */
+		try{
+			connection = DriverManager.getConnection(URL);
+			statement = connection.createStatement();
+			statement.executeUpdate(Query);
+			statement.close();
+			connection.close();
+		}
+		catch(SQLException SQLe){
+			System.out.println("Members.java\n"+SQLe.toString());
+		}
 		}
 	}
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 
-}
