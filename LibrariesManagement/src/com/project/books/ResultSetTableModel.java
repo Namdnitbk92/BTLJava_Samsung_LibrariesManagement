1607587bd735c4a0eb217d02b65427603bf869af
oostarbk <lvdung.bk@gmail.com>
2014-12-02 20:47:24 +0700

    Delete Duplicate ToolBar class, Update class ResultSetTableModel, init class SearchBooksAndMembers and add images

--- LibrariesManagement/src/com/project/books/ResultSetTableModel.java
+++ LibrariesManagement/src/com/project/books/ResultSetTableModel.java
@@ -1,21 +1,79 @@
 package com.project.books;
+
 import javax.swing.table.AbstractTableModel;
 
 import java.sql.*;
-/**
- *Các hàng cột của ResultSet được đếm từ 1 và  các hàng cột của JTable 
- *được đếm từ 0 .Khi xử lí các hàng và cột của ResultSet
- *  cho sử dụng trong JTable, đó là điều cần thiết để thêm 1 
- *necessary to add 1 cho số hàng cột để vận dụng cột ResultSet thích hợp
- * (i.e., cột 0 JTable  là cột 1 của  ResultSet  and hàng 0 của JTable  
- * là hàng 1 của  ResultSet)
- */
+
 public class ResultSetTableModel extends AbstractTableModel {
 	private Connection connection;
 	private Statement statement;
 	private ResultSet resultSet;
 	private ResultSetMetaData metaData;
 	private int numberOfRows;
-	// keep track of database connection status
+
+	// Theo doi trang thai ket noi co so du lieu
 	private boolean connectedToDatabase = false;
+
+	// Khoi tao resultSet
+	// Xac dinh so hang
+	public ResultSetTableModel(String driver, String url, String query)
+			throws SQLException, ClassNotFoundException {
+		Class.forName(driver); // load CSDL
+		connection = DriverManager.getConnection(url); // Ket noi CSDL
+		// Tao Statement de truy van CSDL
+		statement = connection.createStatement(
+				ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+		connectedToDatabase = true; // Cap nhat trang thai ket noi CSDL
+		setQuery(query); // Thiet lap truy van va thuc hien
+	}
+
+	// Lay so luong cot trong ResultSet
+	public int getColumnCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	// Tra ve so luong hang trong ResultSet
+	public int getRowCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	// Co gia tri trong hang va cot cu the
+	public Object getValueAt(int arg0, int arg1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	// Thiet lap moi chuoi truy van CSDL
+	public void setQuery(String query) throws SQLException,
+			IllegalStateException {
+		// Kiem tra ket noi CSDL
+		if (!connectedToDatabase)
+			throw new IllegalStateException("Not Connected to Database");
+		// Chi dinh truy van va thuc hien
+		resultSet = statement.executeQuery(query);
+		// Lay du lieu meta cho ResultSet
+		metaData = resultSet.getMetaData();
+		// Xac dinh so hang trong ResultSet
+		resultSet.last(); // Di chuyen den hang cuoi
+		numberOfRows = resultSet.getRow(); // Lay so luong hang
+		fireTableStructureChanged(); // Thong bao cho JTable la da thay doi
+	}
+
+	// Dong ket noi CSDL
+	public void disconnectFromDatabase() {
+		try {
+			statement.close();
+			connection.close();
+		}
+		// Bat SQLExceptions va in thong bao loi
+		catch (SQLException sqlException) {
+			sqlException.printStackTrace();
+		}
+		// Cap nhap trang thai ket noi CSDL
+		finally {
+			connectedToDatabase = false;
+		}
+	}
 }
--- LibrariesManagement/src/com/project/search/SearchBooksAndMembers.java
+++ LibrariesManagement/src/com/project/search/SearchBooksAndMembers.java
@@ -1,5 +1,122 @@
 package com.project.search;
 
-public class SearchBooksAndMembers {
+import javax.swing.*;
 
+import com.project.books.Books;
+import com.project.books.ListSearchBooks;
+import com.project.members.ListSearchMembers;
+import com.project.members.Members;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class SearchBooksAndMembers extends JInternalFrame {
+
+	/***************************************************************************
+	 *** Khai bao cac bien su dung cho chuong trinh ***
+	 ***************************************************************************/
+
+	// Tao North Panel
+	private JPanel northPanel = new JPanel();
+	// Tao label
+	private JLabel title = new JLabel("Tìm kiếm sách và thành viên");
+
+	// Tao center
+	private JPanel center = new JPanel();
+
+	// Tao Center Panel
+	private JPanel centerBooksPanel = new JPanel();
+	// Tao Internal Panel tren center panel
+	private JPanel searchBooksPanel = new JPanel();
+	// Tao Internal Panel tren center panel
+	private JPanel searchBooksButtonPanel = new JPanel();
+
+	// Tao table
+	private JLabel searchBooksLabel = new JLabel(" Tìm kiếm bằng: ");
+	// Tao JComboBox
+	private JComboBox searchBooksTypes;
+	// Tao mang String[] luu cac bookTypes
+	private String[] booksTypes = { "BookID", "Subject", "Title", "Author",
+			"Publisher", "ISBN" };
+	// Tao label
+	private JLabel booksKey = new JLabel(" Viết từ khóa: ");
+	// Tao text field
+	private JTextField booksKeyTextField = new JTextField();
+	// Tao button
+	private JButton searchBooksButton = new JButton("Tìm kiếm");
+
+	// Tao Center Panel
+	private JPanel centerMembersPanel = new JPanel();
+	// Tao Internal Panel tren center panel
+	private JPanel searchMembersPanel = new JPanel();
+	// Tao Internal Panel tren center panel
+	private JPanel searchMembersButtonPanel = new JPanel();
+
+	// Tao table
+	private JLabel searchMembersLabel = new JLabel(" Tìm kiếm bằng: ");
+	// Tao JComboBox
+	private JComboBox searchMembersTypes;
+	// Tao mang String[] luu cac membersTypes
+	private String[] membersTypes = { "MemberID", "Name", "E-Mail", "Major" };
+	// Tao label
+	private JLabel membersKey = new JLabel(" Viết từ khóa: ");
+	// Tao text field
+	private JTextField membersKeyTextField = new JTextField();
+	// Tao button
+	private JButton searchMembersButton = new JButton("Tìm kiếm");
+
+	// Tao south panel
+	private JPanel southPanel = new JPanel();
+	// Tao button
+	private JButton cancelButton = new JButton("Hủy bỏ");
+
+	// Tao mang String de luu tru du lieu
+	private String[] booksData;
+	private String[] membersData;
+	// Tao cac doi tuong tu lop khac de su dung chung trong ActionListener
+	private ListSearchBooks listBooks;
+	private ListSearchMembers listMembers;
+	private Books book;
+	private Members member;
+
+	// Kiem tra thong tin tu text field
+	public boolean isBooksDataCorrect() {
+		booksData = new String[2];
+		booksData[0] = searchBooksTypes.getSelectedItem().toString();
+		for (int i = 1; i < booksData.length; i++) {
+			if (!booksKeyTextField.getText().equals("")) {
+				if (searchBooksTypes.getSelectedItem().toString()
+						.equals("BookID")) {
+					booksData[i] = booksKeyTextField.getText();
+				} else
+					booksData[i] = "'%" + booksKeyTextField.getText() + "%'";
+			} else
+				return false;
+		}
+		return true;
+	}
+
+	// Kiem tra thong tin tu text field
+	public boolean isMembersDataCorrect() {
+		membersData = new String[2];
+		membersData[0] = searchMembersTypes.getSelectedItem().toString();
+		for (int i = 1; i < membersData.length; i++) {
+			if (!membersKeyTextField.getText().equals("")) {
+				if (searchMembersTypes.getSelectedItem().toString()
+						.equals("MemberID")) {
+					membersData[i] = membersKeyTextField.getText();
+				} else
+					membersData[i] = "'%" + membersKeyTextField.getText()
+							+ "%'";
+			} else
+				return false;
+		}
+		return true;
+	}
+
+	// Phuong thuc khoi tao lop searchBooksAndMembers
+	public SearchBooksAndMembers() {
+
+	}
 }
